5772d16bfe6c366317155874876e4ae4924b2904
marufahmed347 <marufahmed550741@gmail.com>
2025-02-20 09:48:02 +0600

    hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Hero } from './components/home/Hero';
 import { Footer } from './components/share/Footer';
 import { Navbar } from './components/share/Navbar';
 
@@ -6,8 +7,9 @@ function App() {
 
   return (
     <>
-      <main className="font-nunito-sans bg-hero-bg min-h-svh bg-no-repeat bg-top ">
+      <main className="font-nunito-sans bg-[url('hero-bg.png')] min-h-svh bg-no-repeat bg-top ">
         <Navbar/>
+        <Hero/>
         <Footer/>
       </main>
     </>
